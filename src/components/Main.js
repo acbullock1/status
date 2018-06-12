97c0e3f1c59b18bb6a55c6594fa5ec4247443ba4
Alex Bullock <abullock@xevo.com>
2018-06-12 11:20:43 -0400

    updated status

--- src/components/Main.js
+++ src/components/Main.js
@@ -26,31 +26,36 @@ const columns=[{
     dataIndex: 'status',
     key: 'status'
 }];
-const dataSource = [{
+const dataSource = [
+  // {
+  //   key: '1',
+  //   component: 'xiCEP',
+  //   number: 'MGLN-6812',
+  //   title: 'Clickstream CEP Incident Alerting',
+  //   status: 'Waiting for this to be in QA environment before testing. (See Notes Section)'
+  // },
+   {
     key: '1',
-    component: 'xiCEP',
-    number: 'MGLN-6812',
-    title: 'Clickstream CEP Incident Alerting',
-    status: 'Waiting for this to be in QA environment before testing. (See Notes Section)'
-  }, {
-    key: '2',
     component: 'Door Panel UI',
     number: 'MGLN-6948',
     title: 'Panel-UI to send location services to Panel-app',
-    status: "The UI team is making a dev build to test their fix for the socket integration. They are awaiting access to Jenkins before cutting an official production build (the team's lack of Jenkins access is slowing this down)."
+    status: "Keiver/Jay made a dev build. They have resolved the issue and the panel-app IS receiving the location events. However, due to Jenkins issues (see Notes section), an official build has not been generated as yet. I would prefer to validate agains the production build."
 }, {
-  key: '3',
+  key: '2',
   component: 'Compass: OTT Amazon Firestick',
   number: 'MGLN-5679',
   title: 'OV:Fire TV - Create error message for Account Creation and User Login',
   status:'Waiting to see if Ed actually needs help on this, if so, I will need to get a knowledge transfer so I can properly test this component.'
 }];
+
+let now = new Date(2018, 5, 12, 11, 20);
+let lastupdated = now.toDateString() + " - " + now.toLocaleTimeString();
 class Main extends React.Component {
     render() {
       return (
         <div>
-          
-            <Card title={<div>Ticket Status   <Icon type="line-chart"/></div>}  >
+            
+            <Card title={<div>Ticket Status   <Icon type="line-chart"/> <br/><strong>Last Updated:</strong>  {lastupdated}</div>}  >
             
               <Table dataSource={dataSource} columns={columns} size="small" scroll={{ x: 480 }}/>
             </Card>
@@ -60,7 +65,7 @@ class Main extends React.Component {
                 <ul>
                   <li>The team is having issues accessing Jenkins. This, along with some environment issues are preventing new builds from being created, and preventing builds from being promoted to the next environment.</li>
                     <li>Regression on <strong>Door Panel UI</strong> will begin when all tickets are closed and a production build is loaded on the door panel.</li>
-                    <li><a target="_blank" href="https://carnival.atlassian.net/wiki/spaces/MGLN/pages/157278542/QA+ticket+validation+tracking">QA Ticket Validation Tracking</a></li>
+                    <li><a target="_blank" rel="noopener noreferrer" href="https://carnival.atlassian.net/wiki/spaces/MGLN/pages/157278542/QA+ticket+validation+tracking">QA Ticket Validation Tracking</a></li>
                 </ul>
             </Card>
         </div>
