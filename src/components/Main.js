c4b263f64f4d1a5c2e9fe58ff4ccd1a188e56984
Alex Bullock <abullock@xevo.com>
2018-06-14 14:03:05 -0400

    added link for my open issues

--- src/components/Main.js
+++ src/components/Main.js
@@ -63,15 +63,15 @@ const dataSource = [
     status: "QA Blocked: Keiver/Jay made a dev build. They have resolved the issue and the panel-app IS receiving the location events. However, due to Jenkins issues (see Notes section), an official build has not been generated as yet. I would prefer to validate against the production build."
 }, 
 // {
-  // key: '3',
-  // component: 'Compass: OTT Amazon Firestick',
-  // number: 'MGLN-5679',
-  // state: 'QA IN PROGRESS',
-  // title: 'OV:Fire TV - Create error message for Account Creation and User Login',
-  // status:'Pending: Waiting to see if Ed actually needs help on this, if so, I will need to get a knowledge transfer so I can properly test this component. This ticket is currently assigned to Kenny Tang'
+//   key: '3',
+//   component: 'Compass: OTT Amazon Firestick',
+//   number: 'MGLN-5679',
+//   state: 'QA IN PROGRESS',
+//   title: 'OV:Fire TV - Create error message for Account Creation and User Login',
+//   status:'Pending: Waiting to see if Ed actually needs help on this, if so, I will need to get a knowledge transfer so I can properly test this component. This ticket is currently assigned to Kenny Tang'
 // },
 {
-  key:'3',
+  key:'4',
   component: 'xiUserVideoAPI',
   number: 'MGLN-7180',
   state: 'DEV COMPLETE',
@@ -79,14 +79,62 @@ const dataSource = [
   status: 'Changes deployed to DevInt. However, this is currently UNTESTABLE in DevInt due to lack of availability of test-proxy in devInt and lack of availability of xiCMS in DevInt to allow interaction of service through XOS.  Waiting for it to get transitioned to QA in progress'
 },
 {
-  key:'4',
+  key:'5',
   component: 'XOS',
   number:'MGLN-7168',
   state: 'DEV COMPLETE',
   title: "'XOS' /applicationschedule/{ContentId} call to support backward compatibility with the dmuOS for digital signage",
   status: 'Straight-forward test; waiting for the ticket to move to QA IN PROGRESS'
-
-}];
+}
+// {
+//   key:'6',
+//   component:'Compass: OTT Apple TV',
+//   number: 'MGLN-7072',
+//   state: 'QA IN PROGRESS',
+//   title: 'OV:Apple TV - No Recent/Favorite Service available',
+//   status: ''
+// },
+// {
+//   key:'7',
+//   component: 'Compass: OTT Apple TV',
+//   number: 'MGLN-6967',
+//   state: 'QA IN PROGRESS',
+//   title: "OV:Apple TV - Migrate user's favorite and recent",
+//   status:''
+// },
+// {
+//   key:'8',
+//   component: 'Compass: OTT Apple TV',
+//   number: 'MGLN-6966',
+//   state: 'QA IN PROGRESS',
+//   title: "OV:Apple TV - Recent and Favorites storing and fetching and UI",
+//   status:''
+// },
+// {
+//   key:'9',
+//   component: 'Compass: OTT Apple TV',
+//   number: 'MGLN-6965',
+//   state: 'QA IN PROGRESS',
+//   title: "OV:Apple TV - Empty recent and favorite UI - string update",
+//   status:''
+// },
+// {
+//   key:'10',
+//   component: 'Compass: OTT Apple TV',
+//   number: 'MGLN-6962',
+//   state: 'QA IN PROGRESS',
+//   title: "OV:Apple TV - Series Detail Page",
+//   status:''
+// },
+// {
+//   key:'11',
+//   component: 'OTT-RokuTV-1.2.0',
+//   number: 'MGLN-7070',
+//   state: 'DEV COMPLETE',
+//   title: "OV:Roku TV - No Recent/Favorite Service available",
+//   status:''
+// }
+];
 
 let now = new Date(2018, 5, 14, 11, 30);
 let lastupdated = now.toDateString() + " - " + now.toLocaleTimeString();
@@ -108,6 +156,7 @@ class Main extends React.Component {
                   <li>The team is having issues accessing Jenkins. This, along with some environment issues are preventing new builds from being created, and preventing builds from being promoted to the next environment.</li>
                     <li>Regression on <strong>Door Panel UI</strong> will begin when all tickets are closed and a production build is loaded on the door panel.</li>
                     <li><a target="_blank" rel="noopener noreferrer" href="https://carnival.atlassian.net/wiki/spaces/MGLN/pages/157278542/QA+ticket+validation+tracking">QA Ticket Validation Tracking</a></li>
+                    <li><a target="_blank" rel="noopener noreferrer" href="https://carnival.atlassian.net/issues/?jql=filter%20in%20(xt_Magellan_Open_Sprints_In_QA_QA)%20and%20assignee%3Dabullock">Open Issues Assigned To Me</a></li>
                 </ul>
                 </Card>
             </Card>
